b7a70dab145b3beefed96789b33a2407f48545fd
miguelramirez93 <darckzios@gmail.com>
2021-02-20 16:37:04 -0500

    Added timmer to go routine genberation

--- shared/utils/queue_utils/queue_utils.go
+++ shared/utils/queue_utils/queue_utils.go
@@ -5,6 +5,8 @@ import "time"
 // QueueJobWithTimmer queue jobs with a basic fifo queue drive by time intervals
 func QueueJobWithTimmer(millisecondsInterval int, job func()) {
 	timer1 := time.NewTimer(time.Duration(millisecondsInterval) * time.Millisecond)
+	timer2 := time.NewTimer(time.Duration(millisecondsInterval) * time.Millisecond)
+	<-timer2.C
 	go func() {
 		<-timer1.C
 		job()
